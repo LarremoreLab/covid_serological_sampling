56bfe8d6a314e0f869426092cd9bcfd7d9574f79
dblarremore <daniel.larremore@colorado.edu>
2020-04-17 09:08:20 -0600

    SEIR Workbook uploaded

--- codebase/util.py
+++ codebase/util.py
@@ -147,3 +147,22 @@ def lineswap_axis(fig, ax, zorder=-1000, lw=1, alpha=0.2, skip_zero=False):
     ax.tick_params(axis=u'y', which=u'both',length=0)  # Erase ticks by setting length=0
     ax.set_xlim(xmin, xmax)  # Retain original xlims
 
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
